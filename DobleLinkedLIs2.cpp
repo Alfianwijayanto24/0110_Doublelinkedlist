28211ef2d36d5d0a5e83c1fcb5c90fec9821c2b8
Alfianwijayanto24 <alvianwijayanto74@gmail.com>
2025-05-15 18:17:59 +0700

    Step 2b: Output result

--- DobleLinkedLIs2.cpp
+++ DobleLinkedLIs2.cpp
@@ -192,6 +192,19 @@ class DobleLinkedList
         while (current != NULL && current->noMhs != rollNo)
             current = current->next;
 
+            // Step 2b: Output result
+            if (current == NULL)
+            {
+                cout << "Record not found\n";
+
+            }
+            else
+            {
+                cout << " Record found\n";
+                cout << "Roll Number :" << current->noMhs<< endl;
+            }
+            
+
         
         
     }
